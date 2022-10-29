8ba59d853f25781dcb4df511ae95c7c728020434
asadmahmood2000 <asad.mahmood@ontariotechu.net>
2022-10-28 20:38:41 -0400

    fixed capitalization error

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET about page. */
 router.get('/about-me', function(req, res, next) {
-  res.render('About');
+  res.render('about');
 });
 
 module.exports = router;
